27428367e1de22d305d4ceab097ed061ce2cbae4
bekirisgor <bekirisgor@gmail.com>
2020-03-29 04:39:11 +0300

    eslint included

--- client/src/app.js
+++ client/src/app.js
@@ -7,28 +7,32 @@ import { Route, Switch, BrowserRouter as Router, Link } from "react-router-dom";
 import SideBar from "./component/sidebar/sidebar";
 import NavBar from "./component/navbar/navbar";
 import { withRouter } from "react-router";
+import { Container } from "semantic-ui-react";
 
 export const App = () => (
   <div>
-    <NavBar />
-    <SideBar />
-    <Switch>
-      <Route path="/accounts">
-        <AccTable />
-      </Route>
+    <Container>
+      <NavBar />
+      <SideBar />
 
-      <Route path="/locations">
-        <Location />
-      </Route>
+      <Switch>
+        <Route path="/accounts">
+          <AccTable />
+        </Route>
 
-      <Route path="/reviews">
-        <Reviews />
-      </Route>
+        <Route path="/locations">
+          <Location />
+        </Route>
 
-      <Route exact path="/">
-        <Home />
-      </Route>
-    </Switch>
+        <Route path="/reviews">
+          <Reviews />
+        </Route>
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Container>
   </div>
 );
 
--- client/src/container/Reviews/action.js
+++ client/src/container/Reviews/action.js
@@ -4,6 +4,18 @@ export const FETCH_REVIEWS_BEGIN = "FETCH_REVIEWS_BEGIN";
 export const FETCH_REVIEWS_SUCCESS = "FETCH_REVIEWS_SUCCESS";
 export const FETCH_REVIEWS_FAILURE = "FETCH_REVIEWS_FAILURE";
 
+export const fetchReviews = (locationID = "") => {
+  return async function(dispatch) {
+    dispatch(fetchReviewsBegin());
+
+    return await axios
+      .get("/reviews/list/" + encodeURIComponent(locationID))
+      .then(res => dispatch(fetchReviewsSucces(res.data)))
+      .catch(error => {
+        return dispatch(fetchReviewsFailure(error));
+      });
+  };
+};
 // fetch reviews
 const fetchReviewsBegin = () => ({
   type: FETCH_REVIEWS_BEGIN
@@ -18,19 +30,6 @@ const fetchReviewsFailure = error => ({
   type: FETCH_REVIEWS_FAILURE,
   payload: { error }
 });
-
-export const fetchReviews = (locationID = "") => {
-  return async function(dispatch) {
-    dispatch(fetchReviewsBegin());
-
-    return await axios
-      .get("/reviews/list/" + encodeURIComponent(locationID))
-      .then(res => dispatch(fetchReviewsSucces(res.data)))
-      .catch(error => {
-        return dispatch(fetchReviewsFailure(error));
-      });
-  };
-};
 
 export const SEND_REVIEWS_BEGIN = "SEND_REVIEWS_BEGIN";
 export const SEND_REVIEWS_SUCCESS = "SEND_REVIEWS_SUCCESS";
