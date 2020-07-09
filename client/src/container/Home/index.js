9dcf73d40cc2077102aeeab67483470f64fb8abd
bekir işgör <bekirisgor@bekir-MacBook-Pro.local>
2020-07-09 17:33:11 +0300

    z

--- client/src/container/Home/index.js
+++ client/src/container/Home/index.js
@@ -99,7 +99,7 @@ export class Home extends Component {
           <Button
             as="a"
             target="popup"
-            href="https://accounts.google.com/o/oauth2/v2/auth?client_id=66875520156-8gc7rf7vvv4570oeboqgag71c84fcr41.apps.googleusercontent.com&response_type=code&redirect_uri=http://ec2-3-120-181-41.eu-central-1.compute.amazonaws.com:5000%2Foauth2callback&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fbusiness.manage&access_type=offline&include_granted_scopes=true&prompt=consent"
+            href="https://accounts.google.com/o/oauth2/v2/auth?client_id=66875520156-8gc7rf7vvv4570oeboqgag71c84fcr41.apps.googleusercontent.com&response_type=code&redirect_uri=http://ec2-18-196-183-252.eu-central-1.compute.amazonaws.com:5000/%2Foauth2callback&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fbusiness.manage&access_type=offline&include_granted_scopes=true&prompt=consent"
           ></Button>
         </div>
         {/* <div>
